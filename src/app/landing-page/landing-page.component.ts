02bbf98ca91ca295927da0c8e513053f30b1a84a
cochiletSerban <cochiletSerban@gmail.com>
2018-04-28 20:14:54 +0300

    final lp layout

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,40 +1,39 @@
-import { Component, OnInit, Renderer2, ElementRef, ViewChild, ViewChildren } from '@angular/core';
+import { Component, OnInit, Renderer2, ViewChild} from '@angular/core';
 import { GetBgService } from '../services/get-bg.service';
 import { BgArray } from '../objects/BgArray';
 import { Angular2FontawesomeModule } from 'angular2-fontawesome';
 import { MyJqService } from '../services/my-jq.service';
 
+declare var $: any;
 
-declare var $:any;
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css']
 })
+
 export class LandingPageComponent implements OnInit {
 
-  @ViewChildren('fa1 ,fa2,fa3') fa;
+  @ViewChild('fa1') fa1;
+  @ViewChild('fa2') fa2;
 
-  bgs : BgArray = {url:[]};
-  spinOnHover : boolean = false;
-  constructor(private bg : GetBgService, private render: Renderer2,
-     private el:ElementRef ) { }
+  bgs: BgArray = {
+    url: ["https://www.ps4wallpapers.com/wp-content/uploads/2018/01/PS4Wallpapers.com_5a5d669a96788_JessicaNigri.jpg"]
+  };
 
-
+  constructor(private bg: GetBgService, private render: Renderer2) {}
 
   ngOnInit() {
     this.bg.getBg().subscribe(resp => this.bgs = resp);
   }
 
-  onHo(hovering:boolean){
-    console.log(this.fa);
-    if (hovering)
-      $('i').css('animation','spin 1s infinite');
-     //console.log(this.fa);
-    //this.render.setStyle(this.el.nativeElement.querySelector('i'),'animation','fadein 1s infinite');
-    // this.render.setStyle(this.fa._results[2].nativeElement,'animation','fadein 1s infinite');
-    else
-      $('i').css('animation','none');
-    // this.render.setStyle(this.el.nativeElement.querySelector('i'),'animation','none');
+  onHo(hovering: boolean) {
+    if (hovering) {
+      this.render.setStyle(this.fa1.nativeElement, 'animation', 'spin 1s forwards');
+      this.render.setStyle(this.fa2.nativeElement, 'animation', 'spin 1s forwards');
+    } else {
+      this.render.setStyle(this.fa1.nativeElement, 'animation', 'spinBack 1s forwards');
+      this.render.setStyle(this.fa2.nativeElement, 'animation', 'spinBack 1s forwards');
+    }
   }
 }
