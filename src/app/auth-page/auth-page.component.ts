4e9ad8ae9da5d5b6e2aaffbdc547c949c4f24b46
cochiletSerban <cochiletSerban@gmail.com>
2018-04-29 23:37:31 +0300

    fixed bug i never knew i had (zoom)

--- src/app/auth-page/auth-page.component.ts
+++ src/app/auth-page/auth-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { User } from '../objects/User';
 
 @Component({
   selector: 'app-auth-page',
@@ -7,9 +9,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth:AuthService) { }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
 }
