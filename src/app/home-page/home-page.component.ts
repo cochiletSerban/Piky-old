473e4cc1874174a8454df088dec54700782cb4af
cochiletSerban <cochiletSerban@gmail.com>
2018-05-25 01:46:44 +0300

    added a buggy homePage

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, Renderer2, ViewChild } from '@angular/core';
+import { BgArray } from '../objects/BgArray';
+import { GetBgService } from '../services/get-bg.service';
+import '../../../node_modules/masonry-layout/dist/masonry.pkgd.js';
+
+declare var $: any;
+declare var window: any;
+
 
 @Component({
   selector: 'app-home-page',
@@ -6,10 +13,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
-
-  constructor() { }
+  @ViewChild('grid') grid;
+  cacat = 'container cacat';
+  images: BgArray = {
+    url: ['https://www.ps4wallpapers.com/wp-content/uploads/2018/01/PS4Wallpapers.com_5a5d669a96788_JessicaNigri.jpg']
+  };
+  constructor(private bg: GetBgService, private render: Renderer2, private ref: ElementRef) { }
 
   ngOnInit() {
+    this.bg.getBg().subscribe(resp => this.images = resp);
   }
-
+  reflow() {
+    window.dispatchEvent(new Event('resize'));
+  }
 }
