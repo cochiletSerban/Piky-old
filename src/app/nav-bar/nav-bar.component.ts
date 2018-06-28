ba0e5bcdbd0bead274e7fff9b50b8ca880bf89a2
SERBAN COCHILET <cochiletSerban@gmail.com>
2018-06-28 13:32:50 +0300

    fixed tab bug and added login

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Renderer2, ElementRef, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -9,11 +10,11 @@ import { Router } from '@angular/router';
 export class NavBarComponent implements OnInit {
   logged:boolean = false;
   @ViewChild('eye') eye;
-  constructor(private render: Renderer2, private ref: ElementRef,private router: Router) { }
+  constructor(private render: Renderer2, private ref: ElementRef,private router: Router, private auth: AuthService) { }
 
   ngOnInit() {
     this.render.setStyle(this.eye.nativeElement, 'animation', 'flash 5s infinite');
-    
+    this.logged = this.auth.isLogedIn();
   }
 
   takeMeHome() {
