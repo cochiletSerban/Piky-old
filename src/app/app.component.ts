a1e1124e9fbe1550fba712feb7168569c640aa4e
cochiletSerban <cochiletSerban@gmail.com>
2018-10-12 18:34:51 +0300

    added safeUrlPipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,4 +14,8 @@ export class AppComponent implements OnInit {
   constructor(private router: Router) {}
   title = 'app';
   ngOnInit() {}
+
+  routerUrl(): string {
+    return this.router.url;
+  }
 }
